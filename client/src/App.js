78c44d62a1749b569addab6524e8454b5ac1b509
Cyrus Shekari <108636344+cyshek@users.noreply.github.com>
2022-09-04 16:12:19 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
-import io from "socket.io-client";
+import io from "socket.io-client";  
 import { useState } from "react";
 import Chat from "./Chat";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("http://localhost:3001");   //Connect front end to back end
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [room, setRoom] = useState("");
+  const [username, setUsername] = useState("");   //State that sets and mutates name
+  const [room, setRoom] = useState("");   //State that sets and mutates room
   const [showChat, setShowChat] = useState(false);
 
-  const joinRoom = () => {
-    if (username !== "" && room !== "") {
-      socket.emit("join_room", room);
+  const joinRoom = () => {    
+    if (username !== "" && room !== "") {   //Codition that allows user to join a room
+      socket.emit("join_room", room);   //Emit "join_room" event to back end
       setShowChat(true);
     }
   };
@@ -20,8 +20,8 @@ function App() {
   return (
     <div className="App">
       {!showChat ? (
-        <div className="joinChatContainer">
-          <h3>Join A Chat</h3>
+        <div className="joinChatContainer">   //Initial container that user sees, containing input fields that accept name and room ID
+          <h3>Join A Chat</h3>    //Header
           <input
             type="text"
             placeholder="John..."
@@ -36,7 +36,7 @@ function App() {
               setRoom(event.target.value);
             }}
           />
-          <button onClick={joinRoom}>Join A Room</button>
+          <button onClick={joinRoom}>Join A Room</button>  //Listens for click and alerts back end about user's attempt to establish connection
         </div>
       ) : (
         <Chat socket={socket} username={username} room={room} />
