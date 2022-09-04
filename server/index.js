e5b2f273eba7adb0402c06c31e7e303aed62e1f5
Cyrus Shekari <108636344+cyshek@users.noreply.github.com>
2022-09-04 15:53:30 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,19 +2,19 @@ const express = require("express");
 const app = express();
 const http = require("http");   //Need http to build server with Socket.io
 const cors = require("cors");
-const { Server } = require("socket.io");
-app.use(cors());   //Cors is a library from Socket.io that helps detect and resolve bugs
+const { Server } = require("socket.io");    //  
+app.use(cors());   //Cors is a library from node.js that helps detect and resolve bugs
 
 const server = http.createServer(app);    //Create server by passing an express app
 
-const io = new Server(server, {
+const io = new Server(server, { //io is the connection that we will be establishing to server. 
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
 
-io.on("connection", (socket) => {
+io.on("connection", (socket) => {   //Detects (listens for) connection events. When someone connects, we call the callback function print user ID obtained from prop.
   console.log(`User Connected: ${socket.id}`);
 
   socket.on("join_room", (data) => {
@@ -26,7 +26,7 @@ io.on("connection", (socket) => {
     socket.to(data.room).emit("receive_message", data);
   });
 
-  socket.on("disconnect", () => {
+  socket.on("disconnect", () => {   //Listens for when someone disconnects from server
     console.log("User Disconnected", socket.id);
   });
 });
