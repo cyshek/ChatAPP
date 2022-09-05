fe082c23b4affedb426d52dd1cc28db0a947d987
Cyrus Shekari <108636344+cyshek@users.noreply.github.com>
2022-09-05 11:55:17 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,16 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
 
-const socket = io.connect("http://localhost:3001");   //Connect front end to back end
+const socket = io.connect("http://localhost:3001");     //Connect front end to back end
 
 function App() {
-  const [username, setUsername] = useState("");   //State that sets and mutates name
-  const [room, setRoom] = useState("");   //State that sets and mutates room
+  const [username, setUsername] = useState("");     //State that sets and mutates name
+  const [room, setRoom] = useState("");     //State that sets and mutates room
   const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {    
-    if (username !== "" && room !== "") {   //Condition that allows user to join a room
-      socket.emit("join_room", room);   //Emit "join_room" event to back end
+    if (username !== "" && room !== "") {       //Condition that allows user to join a room
+      socket.emit("join_room", room);     //Emit "join_room" event to back end
       setShowChat(true);
     }
   };
@@ -20,7 +20,7 @@ function App() {
   return (
     <div className="App">
       {!showChat ? (
-        <div className="joinChatContainer">   <!-- Initial container that user sees, containing input fields that accept name and room ID -->
+        <div className="joinChatContainer">       <!-- Initial container that user sees, containing input fields that accept name and room ID -->
           <h3>Join A Chat</h3>   
           <input
             type="text"
@@ -36,7 +36,7 @@ function App() {
               setRoom(event.target.value);
             }}
           />
-          <button onClick={joinRoom}>Join A Room</button>  <!-- Listens for click and alerts back end about user's attempt to establish connection -->
+          <button onClick={joinRoom}>Join A Room</button>       <!-- Listens for click and alerts back end about user's attempt to establish connection -->
         </div>
       ) : (
         <Chat socket={socket} username={username} room={room} />
